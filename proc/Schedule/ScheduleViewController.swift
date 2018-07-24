770a16ea4abcd44ad02f971534b6ebb6047d3756
swift_7/3 <cisspmit@naver.com>
2018-07-24 14:22:27 +0900

    파일로 묶음; ㅂq q Q

---
+++ proc/Schedule/ScheduleViewController.swift
@@ -0,0 +1,11 @@
+//import JTAppleCalendar
+//import Foundation
+//
+//class ScheduleViewController :UIViewController {
+//    @IBOutlet weak var calenderView: JTAppleCalendarView!
+//    @IBOutlet weak var tableView: UITableView!
+//    @IBOutlet weak var showTodayButton: UIBarButtonItem!
+//    @IBOutlet weak var separatorViewTopConstraint: NSLayoutConstraint!
+//
+//    var scheduleGroup : [String:[]
+//}
