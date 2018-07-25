025f2d18bd814641aabe7f56cc373670f2bc5d0c
lee-yujinn <leeyj7988@naver.com>
2018-07-25 18:29:38 +0900

    라디오버튼 1차추가

---
+++ proc/Store/Radiobutton.swift
@@ -0,0 +1,43 @@
+import UIKit
+import Foundation
+
+class RadioButton: UIButton {
+    var alternateButton:Array<RadioButton>?
+    
+    override func awakeFromNib() {
+        self.layer.cornerRadius = 5
+        self.layer.borderWidth = 2.0
+        self.layer.masksToBounds = true
+    }
+    
+    func unselectAlternateButtons(){
+        if alternateButton != nil {
+            self.isSelected = true
+            
+            for aButton:RadioButton in alternateButton! {
+                aButton.isSelected = false
+            }
+        }else{
+            toggleButton()
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        unselectAlternateButtons()
+        super.touchesBegan(touches, with: event)
+    }
+    
+    func toggleButton(){
+        self.isSelected = !isSelected
+    }
+    
+    override var isSelected: Bool {
+        didSet {
+            if isSelected {
+                //self.layer.borderColor = Color.turquoise.cgColor
+            } else {
+                //self.layer.borderColor = Color.grey_99.cgColor
+            }
+        }
+    }
+}
