784058f9c12a5ea8a77392d85b298625fd09fc43
yulimlimlim <tjreb@naver.com>
2018-07-25 19:23:16 +0900

    재고 데이터 추가, 저장소 필요

--- proc/Store/AddStoreViewController.swift
+++ proc/Store/AddStoreViewController.swift
@@ -14,10 +14,10 @@ class AddStoreViewController:UIViewController, UITextFieldDelegate {
     @IBOutlet weak var AddlblName :UITextField!
     @IBOutlet weak var AddlblMany :UITextField!
     @IBOutlet weak var AddlblDate: UITextField!
-    @IBOutlet weak var AddbtnSave: UIButton!
+    @IBOutlet weak var AddbtnSave: UIButton! //저장방법
     @IBOutlet weak var AddlblCall: UITextField!
     
-    @IBOutlet weak var AddbtnFinish: UIButton!
+    @IBOutlet weak var AddbtnFinish: UIButton! //완료
     
     
     // 화면 아무곳 누르면 키보드 내려감
@@ -28,11 +28,41 @@ class AddStoreViewController:UIViewController, UITextFieldDelegate {
     //키보드 엔터키 누르면 키보드 내려감
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
+        print("바보")
         return true
     }
     
+/*
     @IBAction func getValue () {
         var Nametext:String = AddlblName.text!
         var Manytext:String = AddlblMany.text!
+        var DateAdd:String = AddlblDate.text!
+        var CallAdd:String = AddlblCall.text!
     }
+ */
+    
+    //버튼 클릭했을 때 함수
+    @IBAction func saveAddInfoBtn(_ sender: AnyObject) {
+        var test = StoreModel_More.init()
+        var infoDateAdd:String = HomeViewCtroller.dateInfo()
+        
+        var NameAdd:String = AddlblName.text!
+        var ManyAdd:Int = Int(AddlblMany.text!)!
+        var DateAdd:String = AddlblDate.text!
+        var CallAdd:String = AddlblCall.text!
+        var SaveAdd:saveStyle = .Cold
+        var TtManyAdd:Int = (30 + ManyAdd)
+        
+        //시험용 배열 데이터 선언
+        var addStock = Store(name: NameAdd, UpDate: infoDateAdd, DownDate: DateAdd, many: ManyAdd, saveStyle: SaveAdd, TotalMany: TtManyAdd, Call: CallAdd)
+        
+        // 데이터 받기, 출력 확인
+//        print(addStock)
+//
+//        test.arrayList.append(addStock)
+//        for i in 0..<test.arrayList.count {
+//            print(test.arrayList[i])
+//        }
+//    }
+    
 }
