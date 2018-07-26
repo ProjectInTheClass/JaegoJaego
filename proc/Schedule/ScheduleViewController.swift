363490e04d9553542de712890da9cfdd5f426252
swift_7/3 <cisspmit@naver.com>
2018-07-26 19:04:42 +0900

    재고 업데이트

--- proc/Schedule/ScheduleViewController.swift
+++ proc/Schedule/ScheduleViewController.swift
@@ -8,19 +8,6 @@ import FSCalendar
 class ScheduleViewController :UIViewController, FSCalendarDataSource, FSCalendarDelegate {
 
     @IBOutlet weak var todayDate :UILabel?
-    
-//    let scheduleDate = HomeViewController.dateInfo_Date()
-//    
-//    if let date = scheduleDate {
-//        todayDate?.text = scheduleDate
-//        
-//    }
-    
     @IBOutlet weak var ourCalendar: FSCalendar!
 
-    //let newCalendar = FSCalendar().
-    
-//    @IBAction func tempbutton(_ sender: AnyObject){
-//        print(newCalendar)
-//    }
 }
