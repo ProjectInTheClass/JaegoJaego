ce2d190fc8bd44772950deb7afa135a818e60857
yulimlimlim <tjreb@naver.com>
2018-07-30 16:59:46 +0900

    네비게이션바 투명, UI 수정

--- proc/Store/StoreViewController.swift
+++ proc/Store/StoreViewController.swift
@@ -1,11 +1,25 @@
-//import UIKit
-//import Foundation
-//
-//class StoreViewController : UIViewController {
-//
-//    var datafilePath: String?
-//    @IBAction func saveDate(sender: AnyObject) {
-//        
-//    }
-//    
-//}
+import UIKit
+
+
+class StoreViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setNavigationBar() //네비바 셋팅
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setNavigationBar(){
+        let bar:UINavigationBar! =  self.navigationController?.navigationBar
+        bar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        bar.shadowImage = UIImage()
+        bar.backgroundColor = UIColor.clear
+    }
+    
+}
+
